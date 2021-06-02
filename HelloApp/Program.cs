905b612fac20568f6dc5eccc5660cb5ba9c39899
dreameralexander@yandex.ru <dreameralexander@yandex.ru>
2021-06-02 09:40:56 +0300

    Продолжаю изучать литералы, целочисленые и вещественные переменные

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -57,7 +57,7 @@ namespace HelloApp
             Console.WriteLine("\nНасчет представления в двоичной системе, все несколько иначе, 00000000 это false, а вот любой другой вариант это true.");
 
             //Целочисленные литералы
-            byte temperature = 20;
+            sbyte temperature = 20;
             Console.Write($"Температура летом около {temperature}, это {Convert.ToString(temperature, 2).PadLeft(8, '0')} в двоичной системе.");
 
             short meltingTemperature = 1500;
@@ -68,6 +68,39 @@ namespace HelloApp
 
             meltingTemperature = 0x0D48; // задаем значение переменной в 16-ой системе.
             Console.Write($"\nТемпература плавления вольфрама около {meltingTemperature}, это {Convert.ToString(meltingTemperature, 2).PadLeft(16, '0')} в двоичной системе.");
+
+            byte countStudentsInClass = 25;
+            Console.Write($"\nВ 8А учиться {countStudentsInClass} учеников");
+
+            ushort numberIslandsInIndonesia = 17804;
+            Console.Write($"\nВ Кол-во островов в Индонезии {numberIslandsInIndonesia}");
+
+            uint populationOfAmericans = 327167000U;
+            Console.Write($"\nВ Численость населения в США {populationOfAmericans}");
+
+            int populationGrowthInUkraineIn2020 = -159661;
+            Console.Write($"\nВ Прирост числености населения в Украине {populationGrowthInUkraineIn2020}");
+
+            ulong populationOfWorld = 7866000000UL;
+            Console.Write($"\nВ Численость населения в мире {populationOfWorld}");
+
+            long populationGrowthInWorldIn2020 = 81000000L;
+            Console.Write($"\nВ Прирост числености населения в мире {populationGrowthInWorldIn2020}");
+
+            //Вещественные литералы
+            float valuePi = 3.14F;
+            valuePi = 314E-2F;
+            Console.Write($"\nЗначение ПИ равно - {valuePi}");
+
+            double gdpRussia2020InTrillionRub = 106.6066;
+            gdpRussia2020InTrillionRub = 1.066066E2;
+            Console.Write($"\nВВП России за 2020 в триллионах рос. р. - {gdpRussia2020InTrillionRub}");
+
+            decimal curentDollarToEuroExchangeRate = 1.2212M;
+            curentDollarToEuroExchangeRate = 12212E-4M;
+            Console.Write($"\nТекущий курс USD к EURO - {curentDollarToEuroExchangeRate}");
+
+
 
             // Ждем ввода пользователя
             Console.ReadKey();
